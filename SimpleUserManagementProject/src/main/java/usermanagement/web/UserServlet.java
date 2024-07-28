1be83077b73590bd2da1e394acdc7ed5d54090a2
ManolisKoulalis <manoliskoulalis@gmail.com>
2024-07-28 17:28:27 +0300

    Add files via upload

---
+++ SimpleUserManagementProject/src/main/java/usermanagement/web/UserServlet.java
@@ -0,0 +1,132 @@
+package usermanagement.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import usermanagement.dao.UserDao;
+import usermanagement.model.User;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	protected UserDao userDao;
+	
+    
+    public UserServlet() {
+      this.userDao=new UserDao();
+    }
+
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertUser(request, response);
+				break;
+			case "/delete":
+				deleteUser(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				updateUser(request, response);
+				break;
+			default:
+				listUser(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}	
+		
+		
+	}
+
+	
+	// in the case that we will choose to create a new user
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void insertUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+		User newUser = new User(name, email, country);
+		userDao.insertUser(newUser);
+		response.sendRedirect("list");
+	}
+	
+	//delete a user
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		userDao.deleteUser(id);
+		response.sendRedirect("list");
+	}
+	
+	
+	// to show the edit page and to give the user details as we do not want to create him from the scratch
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		User existingUser = userDao.selectUser(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		request.setAttribute("user", existingUser);
+		dispatcher.forward(request, response);
+
+	}
+	
+	//the action of updating tha user
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+
+		User user = new User(id, name, email, country);
+		userDao.updateUser(user);
+		response.sendRedirect("list");
+	}
+	
+	
+	private void listUser(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		List<User> listUser = userDao.selectAllUsers();
+		request.setAttribute("listUser", listUser);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+
+	
+
+}
